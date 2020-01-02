237dcc66382a17356a28f4f892e7075e6e8d76b9
Lianzhen <lianzhenw01@gmail.com>
2020-01-02 19:33:24 +0800

    update

--- app.py
+++ app.py
@@ -37,10 +37,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='hi'))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
